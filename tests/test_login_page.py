de35715cb2372c56c910ab35f05fd0871646d613
sxseewhoya <savasava23879@gmail.com>
2025-02-24 21:30:48 +0300

    qq

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,7 +1,17 @@
+from locators.login_page_locators import LOGIN_FIELD, PASSWORD_FIELD
+from pages.base_page import BasePage
+
+
 class TestLoginPage:
 
     def test_hp_login(self, login_playwright):
-        from locators.login_page_locators import LOGIN_FIELD
         login_playwright.input_text(locator=LOGIN_FIELD, text='standard-user')
-        from locators.login_page_locators import PASSWORD_FIELD
         login_playwright.input_text(locator=PASSWORD_FIELD, text='secret_sauce')
+
+class TestProductsPage:
+
+    def test_burger_menu_items(self, login_playwright, page):
+        login_playwright.input_text(locator=LOGIN_FIELD, text='standard-user')
+        login_playwright.input_text(locator=PASSWORD_FIELD, text='secret_sauce')
+        items = BasePage(page)
+        assert items.find_all_items()
