c072a408416914b7df043f1c9ba12c8baea89992
sxseewhoya <savasava23879@gmail.com>
2025-02-24 20:36:55 +0300

    finallyy

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,8 +1,10 @@
-from locators.login_page_locators import *
+
 
 
 class TestLoginPage:
 
     def test_hp_login(self, login_playwright):
+        from locators.login_page_locators import LOGIN_FIELD
         login_playwright.input_text(locator=LOGIN_FIELD, text='standard-user')
+        from locators.login_page_locators import PASSWORD_FIELD
         login_playwright.input_text(locator=PASSWORD_FIELD, text='secret_sauce')
